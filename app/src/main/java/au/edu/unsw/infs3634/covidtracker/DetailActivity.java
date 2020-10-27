d72a708d4355948fed60178781c0c0264fba910d
yooie22 <59639797+yooie22@users.noreply.github.com>
2020-10-27 20:43:01 +1100

    Initial commit

---
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/DetailActivity.java
@@ -0,0 +1,76 @@
+package au.edu.unsw.infs3634.covidtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailActivity extends AppCompatActivity {
+    public static final String INTENT_MESSAGE = "au.edu.unsw.infs3634.covidtracker.intent_message";
+
+    private TextView mCountry;
+    private TextView mNewCases;
+    private TextView mTotalCases;
+    private TextView mNewDeaths;
+    private TextView mTotalDeaths;
+    private TextView mNewRecovered;
+    private TextView mTotalRecovered;
+    private ImageView mSearch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        mCountry = findViewById(R.id.tvCountry);
+        mNewCases = findViewById(R.id.tvNewCases);
+        mTotalCases = findViewById(R.id.tvTotalCases);
+        mNewDeaths = findViewById(R.id.tvNewDeaths);
+        mTotalDeaths = findViewById(R.id.tvTotalDeaths);
+        mNewRecovered = findViewById(R.id.tvNewRecovered);
+        mTotalRecovered = findViewById(R.id.tvTotalRecovered);
+        mSearch = findViewById(R.id.ivSearch);
+
+        Intent intent = getIntent();
+        String countryCode = intent.getStringExtra(INTENT_MESSAGE);
+
+        Gson gson = new Gson();
+        Response response = gson.fromJson(Response.json, Response.class);
+
+        List<Country> countries = response.getCountries();
+        for(final Country country : countries) {
+            if(country.getCountryCode().equals(countryCode)) {
+                DecimalFormat df = new DecimalFormat( "#,###,###,###" );
+                setTitle(country.getCountryCode());
+                mCountry.setText(country.getCountry());
+                mNewCases.setText(df.format(country.getNewConfirmed()));
+                mTotalCases.setText(df.format(country.getTotalConfirmed()));
+                mNewDeaths.setText(df.format(country.getNewDeaths()));
+                mTotalDeaths.setText(df.format(country.getTotalDeaths()));
+                mNewRecovered.setText(df.format(country.getNewRecovered()));
+                mTotalRecovered.setText(df.format(country.getTotalRecovered()));
+                mSearch.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        searchCountry(country.getCountry());
+                    }
+                });
+            }
+        }
+    }
+
+    private void searchCountry(String country) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=covid " + country));
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/au/edu/unsw/infs3634/covidtracker/MainActivity.java
@@ -0,0 +1,85 @@
+package au.edu.unsw.infs3634.covidtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private CountryAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.rvList);
+        mRecyclerView.setHasFixedSize(true);
+        CountryAdapter.RecyclerViewClickListener listener = new CountryAdapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, String countryCode) {
+                launchDetailActivity(countryCode);
+            }
+        };
+
+        //Use Gson Library to convert json data to java class
+        Gson gson = new Gson();
+        Response response = gson.fromJson(Response.json, Response.class);
+        mAdapter = new CountryAdapter(response.getCountries(), listener);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void launchDetailActivity(String message) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(DetailActivity.INTENT_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                mAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sort_new:
+                mAdapter.sort(CountryAdapter.SORT_METHOD_NEW);
+                return true;
+            case R.id.sort_total:
+                mAdapter.sort(CountryAdapter.SORT_METHOD_TOTAL);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+}
